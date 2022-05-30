4e2007b4146b28ae0ed9408606b5c8c0d194994e
ChuDucAnh242002 <anh.chu@tuni.fi>
2022-05-30 13:37:35 +0300

    add rect

--- brick.py
+++ brick.py
@@ -1,15 +1,15 @@
+import pygame
+
 class Brick:
     def __init__(self, x, y, width, height, image, health):
-        self.x = x
-        self.y = y
+        self.x = self.original_x = x
+        self.y = self.original_y = y
         self.width = width
         self.height = height
         self.image = image
         self.cur_image = image[0]
         self.health = health
+        self.rect = pygame.Rect(x + 28, y + 35, width , height)
 
     def draw(self, win):
         win.blit(self.cur_image, (self.x, self.y))
-
-
-
